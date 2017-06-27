917edb2fc063593b4068181a31f8fc12a5f4ec88
Carleslc <lazaro.costa.carles@gmail.com>
2017-06-28 00:10:18 +0200

    Added gitignore

--- NeuronalNetwork/bin/me/carleslc/transforms/StaticTransformation.kt
+++
@@ -1,7 +0,0 @@
-package me.carleslc.transforms
-
-class StaticTransformation<I, O>(private val value: O) : Transformation<I, O> {
-	
-	override operator fun invoke(input: I): O = value
-	
-}
--- TicTacToe/bin/me/carleslc/game/Player.kt
+++
@@ -1,7 +0,0 @@
-package me.carleslc.game
-
-enum class Player {
-	X, O;
-	
-	fun opponent(): Player = if (this == X) O else X
-}
